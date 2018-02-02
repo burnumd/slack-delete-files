5542136fb5c0d27e5d580d700a2a291415954f0f
Francis Fernandez <burnumd+github@gmail.com>
2018-02-01 21:18:14 -0500

    Specify days from a prompt, switch to ES6, and lint according to airbnb

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
 #!/usr/bin/env node
-'use strict'
 
-const got = require('got')
-const inquirer = require('inquirer')
-const API_URL = 'https://slack.com/api'
+const got = require('got');
+const inquirer = require('inquirer');
 
-function run (token, options) {
-  const thirtyDaysAgo = Math.floor(new Date().getTime() / 1000) - 30 * 86400
+const API_URL = 'https://slack.com/api';
+
+const filterFiles = (files, options) => {
+  if (!files) {
+    console.warn(`There are no files to be deleted. Either there were no files older than ${options.daysOld} or there is something wrong with your token.`);
+    return [];
+  }
+
+  const removePinned = fileList => fileList.filter(file => !file.pinned_to);
+  const filesToDelete = options.keepPinned
+    ? removePinned(files)
+    : files;
+
+  if (!filesToDelete.length) {
+    console.warn('There are no files to be deleted.');
+  }
+
+  return filesToDelete;
+};
+
+const deleteFiles = (token, files) => {
+  console.log(`Deleting ${files.length} file(s)...`);
+
+  files.forEach(file => got(`${API_URL}/files.delete`, {
+    body: {
+      token,
+      file: file.id,
+    },
+  }).then(() => console.log(`${file.name} was deleted.`)).catch(error => console.error('Error while deleting files.', error)));
+};
+
+const run = (token, options) => {
+  const deleteOlderThan = Math.floor(new Date().getTime() / 1000) - (options.daysOld * 86400);
 
   got(`${API_URL}/files.list`, {
     body: {
       token,
-      ts_to: options.oldOnly ? thirtyDaysAgo : 0,
-      count: 1000
+      ts_to: deleteOlderThan,
+      count: 1000,
     },
-    json: true
+    json: true,
   })
     .then(response => filterFiles(response.body.files, options))
     .then(files => deleteFiles(token, files))
-    .catch(console.error)
-}
+    .catch(console.error);
+};
 
-function filterFiles (files, options) {
-  if (!files) {
-    throw  'Unexpected response from API. Try generating another token!'
-  }
-
-  const removePinned = files => files.filter(file => !file.pinned_to)
-  const filesToDelete = options.keepPinned ? removePinned(files) : files
-
-  if (!filesToDelete.length) {
-    throw  'There are no files to be deleted.'
-  }
-
-  return filesToDelete
-}
-
-function deleteFiles (token, files) {
-  console.log(`Deleting ${files.length} file(s)...`)
-
-  files.forEach(file =>
-    got(`${API_URL}/files.delete`, { body: { token, file: file.id } })
-      .then(() => console.log(`${file.name} was deleted.`))
-      .catch(error => console.error('Error while deleting files.', error))
-  )
-}
-
-exports.filterFiles = filterFiles
-
-inquirer.prompt([{
-  message: 'Please, enter your Slack token.',
-  name: 'token',
-  type: 'input'
-}, {
-  message: 'Delete ONLY files older than 30 days?',
-  name: 'oldOnly',
-  type: 'confirm',
-  default: false
-}, {
-  message: 'Keep pinned files?',
-  name: 'keepPinned',
-  type: 'confirm',
-  default: false
-}])
+inquirer.prompt([
+  {
+    message: 'Please, enter your Slack token.',
+    name: 'token',
+    type: 'password',
+  }, {
+    message: 'How many days old should files be before being deleted (0 means all files will b' +
+        'e deleted)?',
+    name: 'daysOld',
+    type: 'input',
+    validate: value => Number.isInteger(parseInt(value, 10)),
+    default: 30,
+  }, {
+    message: 'Keep pinned files?',
+    name: 'keepPinned',
+    type: 'confirm',
+    default: false,
+  },
+])
   .then(answers => run(answers.token, {
-    oldOnly: answers.oldOnly,
-    keepPinned: answers.keepPinned
+    daysOld: answers.daysOld,
+    keepPinned: answers.keepPinned,
   }))
-  .catch(error => console.error('Error while asking for token.', error))
+  .catch(error => console.error('Error while asking for token.', error));
